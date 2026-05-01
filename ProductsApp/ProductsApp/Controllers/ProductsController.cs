2b8a38b422f1ae1e45b27fea863bef4d32b8d560
Kenny Farinre <tur95645@temple.edu>
2026-04-30 22:16:34 -0400

    update API values

---
+++ ProductsApp/ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "flatato Soup", Category = "Grockeries", Price = 21 },
+            new Product { Id = 2, Name = "bo-yo", Category = "moys", Price = 3.75124M },
+            new Product { Id = 3, Name = "Hashmer", Category = "Harhgdware", Price = 162.99M }
+        };
+
+        public ProductsController()
+        {
+        }
+        public ProductsController(Product[] products)
+        {
+            this.products = products;
+        }
+
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
